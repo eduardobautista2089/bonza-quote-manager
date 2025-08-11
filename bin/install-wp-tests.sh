69ab3cf10ef6b9d1c949e0dde63bd7eff4a118e8
eduardo <eduardobautista2089@gmail.com>
2025-08-11 09:41:25 +0800

    Fix typo in workflow file

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -9,51 +9,51 @@ DB_NAME=$1
 DB_USER=$2
 DB_PASS=$3
 DB_HOST=${4-localhost}
-DB_VERSION=${5-latest}
+WP_VERSION=${5-latest}
 
 set -ex
 
-WP_TEST_DIR=${WP_TEST_DIR-/tmp/wordpress-tests-lib}
+WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
 WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress}
 
 download() {
-    if [ `which curl` ]; then
-        curl -s "$1" > "$2";
-    elif [ `which wget` ]; then
-        wget -nv -0 "$2" "$1"
+    if command -v curl >/dev/null 2>&1; then
+        curl -s "$1" > "$2"
+    elif command -v wget >/dev/null 2>&1; then
+        wget -nv -O "$2" "$1"
     fi
 }
 
 install_wp() {
-    if [-d $WP_CORE_DIR ]; then
-        return;
+    if [ -d "$WP_CORE_DIR" ]; then
+        return
     fi
 
-    mkdir -p $WP_CORE_DIR
+    mkdir -p "$WP_CORE_DIR"
 
-    if ["$WP_VERSION" == "latest" ]; then
+    if [ "$WP_VERSION" == "latest" ]; then
         local ARCHIVE_NAME='latest'
     else
         local ARCHIVE_NAME="wordpress-$WP_VERSION"
     fi
 
-    download https://wordpress.org/${ARCHIVE_NAME}.tar.gz /tmp/wordpress.tar.gz
-    tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
+    download "https://wordpress.org/${ARCHIVE_NAME}.tar.gz" /tmp/wordpress.tar.gz
+    tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C "$WP_CORE_DIR"
 }
 
 install_test_suite() {
-    if [ -d $WP_TESTS_DIR ]; then
-        return;
+    if [ -d "$WP_TESTS_DIR" ]; then
+        return
     fi
 
-    mkdir -p $WP_TESTS_DIR
-    svn co --quiet https://develop.svn.wordpress.org/tags/$(wp core version)/tests/phpunit.includes/ $WP_TESTS_DIR/includes
-    svn co --quiet https://develop.svn.wordpress.org/tags/$(wp core version)/tests/phpunit/data/ $WP_TESTS_DIR/data
+    mkdir -p "$WP_TESTS_DIR"
+
+    svn co --quiet "https://develop.svn.wordpress.org/tags/$(wp core version)/tests/phpunit/includes/" "$WP_TESTS_DIR/includes"
+    svn co --quiet "https://develop.svn.wordpress.org/tags/$(wp core version)/tests/phpunit/data/" "$WP_TESTS_DIR/data"
 }
 
 install_db() {
-    #create database
-    mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS" --host="$DB_HOST" --silent
+    mysqladmin create "$DB_NAME" --user="$DB_USER" --password="$DB_PASS" --host="$DB_HOST" --silent || true
 }
 
 install_wp
